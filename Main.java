b9c89dfc6e1e5e253c8d425fabd2d7f909474ef9
Léa Paulus <leapaulus28@gmail.com>
2012-08-30 12:26:11 +0200

    Add skeleton for alreadyTaken

--- Main.java
+++ Main.java
@@ -1,35 +1,42 @@
 public class Main {
+  private static ArrayList letterList;
+  private static boolean alreadyTaken (char c) {
+    return false;
+  }
   public static void main (String[] args) {
-    // Création du mot
+    // Creation of word
     java.util.Scanner s = new java.util.Scanner(System.in);
     Word w = new Word ();
     int chance=8;
-    while (chance > 0 && !w.all_found()) {
-      // Affichage
-      System.out.println("You still have "+chance+" try");
+    while (chance > 0 && !w.allFound()) {
+      // display
+      System.out.println("You still have " + chance + " try");
       w.display();  
-      // Demande d'une lettre
+      // ask letter
       System.out.print("Give me a letter : ");
       String token = s.next();
-      while (token.length() != 1) {
-        System.out.print("Only one please... : ");
+      while (token.length() != 1 || alreadyTaken(token.charAt(0))) {
+        if (token.length() != 1) {
+          System.out.print("Only one please... : ");
+        } else {
+          System.out.print("You already said it : ");
+        }
         token = s.next();
       }
       char chosen_letter = token.charAt(0);
-      // Vérification et traitement
+      // check and treat
       if (w.checkLetter(chosen_letter)) {
         System.out.println("Well done.");
-      }
-      else {
+      } else {
         System.out.println("Nice try.");
         chance=chance-1;
       }
     }
-    // Message d'au revoir
-    if (w.all_found()) {
+    // goodbye message
+    if (w.allFound()) {
      System.out.println("You win !!!");
-    }
-     else {System.out.println("You lose");
+    } else {
+      System.out.println("You lose");
     }
   }
 }
--- Word.java
+++ Word.java
@@ -1,37 +1,33 @@
 public class Word {
   private String looking = "amour";
   private boolean[] found = new boolean[5];
-  public boolean all_found () {
+  public boolean allFound () {
     int check = 0;
     for (int i = 0; i < found.length; i++) {
       if (found[i] == false) {
         check++;
       }
     }
-    return (check==0);
+    return (check == 0);
   }
   public void display () {
-    int i=0;
-    while (looking.length() > i) {
+    for (int i = 0; i < looking.length(); i++) {
       if (found[i]) {
         System.out.print(looking.charAt(i));
       } else {
         System.out.print("_");
       }
-      i=i+1;
     }
     System.out.println();
   }
   public boolean checkLetter (char l) {
-    int i=0;
-    int passage=0;
-    while (looking.length() > i) {
+    int passage = 0;
+    for (int i = 0; i < looking.length(); i++) {
       if (l == looking.charAt(i)) {
         found[i] = true;
-        passage=passage+1;
+        passage++;
       }
-      i=i+1;
     }
-    return (passage>0);
+    return (0 < passage);
   }
 }
